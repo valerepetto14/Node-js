da085259ba59b4d9d12f91fd65e0a31b53235add
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-13 15:20:46 -0300

    add more content

--- api_github_gist/app.js
+++ api_github_gist/app.js
@@ -7,27 +7,59 @@ app.set('port',3000);
 axios.defaults.headers.get['Authorization'] = "token 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"
 
 
-app.get("/", async (req,res)=>{
-    try{
-        const lista = []
-        const respuesta = await axios.get("https://api.github.com/gists/298e0b8dce371795d9a4e79ee24b8b91")
+//end point usando async awai
+// app.get("/:id", async (req,res)=>{
+//     try{
+//         const id_gist = req.params.id
+//         const lista = []
+//         const respuesta = await axios.get(`https://api.github.com/gists/${id_gist}`)
+//         for (let i in respuesta.data.files){
+//             lista.push(respuesta.data.files[i])
+//         }
+//         const url_content = lista[0].raw_url
+//         console.log(url_content)
+//         const content = await axios.get(url_content)
+//         const id = respuesta.data.id
+//         const owner = respuesta.data.owner.login
+//         res.status(200).json({
+//             "id": id,
+//             "owner": owner,
+//             "content": content.data
+//         })
+//     }catch(err) {
+//         res.send(err)
+//     }
+// })
+
+// endpoint usando .then y .catch
+app.get("/:id", (req,res)=>{
+    const id_gist = req.params.id
+    const lista = []
+    axios.get(`https://api.github.com/gists/${id_gist}`)
+    .then(respuesta => {
         for (let i in respuesta.data.files){
             lista.push(respuesta.data.files[i])
         }
         const url_content = lista[0].raw_url
         console.log(url_content)
-        const content = await axios.get(url_content)
-        const id = respuesta.data.id
-        res.status(200).json({
-            "id":id,
-            "owner":"owner",
-            "content":content.data
+        axios.get(url_content).then(content => {
+            const id = respuesta.data.id
+            const owner = respuesta.data.owner.login
+            const img_owner = respuesta.data.owner.avatar_url
+            res.status(200).json({
+                "id": id,
+                "owner": owner,
+                "avatar": img_owner,
+                "content": content.data
+            })
         })
-    }catch(err) {
-        res.send(err)
-    }
+        .catch(error => res.send(error.response.status))
+    })
+    .catch(error => res.send(error.response.status))
 })
+
 
 app.listen(app.get('port'), ()=>{
     console.log("escuchando.....")
 })
+
