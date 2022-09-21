1fef53c59332cfd63c57c2aaabf03c72cc8c35da
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-21 01:20:26 -0300

    add test

---
+++ test/par.test.js
@@ -0,0 +1,13 @@
+const { par_impar } = require("../funciones_test/funciones_num")
+
+test("par", () => {
+    expect(par_impar(2)).toBe('par')
+    expect(par_impar(4)).toBe('par')
+    expect(par_impar(6)).toBe('par')
+})
+
+test("impar", () => {
+    expect(par_impar(5)).toBe('impar')
+    expect(par_impar(7)).toBe('impar')
+    expect(par_impar(9)).toBe('impar')
+})
