d4f47ba1374c47824d4b5e79121eeaed95ac2471
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-20 23:49:24 -0300

    add yml

--- deserealizar_serealizar/index.js
+++ deserealizar_serealizar/index.js
@@ -1,7 +1,11 @@
-const json = '{"name": "valentin","apellido": "repetto"}'
+const json = {"name": "valentin","apellido": "repetto"}
 
-const obj = JSON.parse(json)
-const obj_txt = JSON.stringify(obj)
-console.log(obj)
-console.log(obj_txt.length)
+
+console.log("serealizar")
+const obj_txt = JSON.stringify(json)
+console.log(obj_txt)
+
+console.log("deserealizar")
+const obj_des = JSON.parse(obj_txt)
+console.log(obj_des)
 
